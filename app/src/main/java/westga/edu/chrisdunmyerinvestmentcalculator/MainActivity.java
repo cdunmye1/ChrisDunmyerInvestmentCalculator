afda6bb8165cf84a1a485c2abf1a06e67c0bb5a9
Chris Dunmyer <cdunmye1@my.westga.edu>
2016-03-09 21:57:42 -0500

    Removing an import that is not needed from MainActivity.java

--- app/src/main/java/westga/edu/chrisdunmyerinvestmentcalculator/MainActivity.java
+++ app/src/main/java/westga/edu/chrisdunmyerinvestmentcalculator/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
